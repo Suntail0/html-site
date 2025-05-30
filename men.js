d8437995f5a1ca9a3ed5afb509f86e1f0ae59947
suntail <alexandra.mistode@gmail.com>
2025-05-30 10:25:50 +0300

    no kids

---
+++ men.js
@@ -0,0 +1,20 @@
+fetch('products.json')
+  .then(response => response.json())
+  .then(products => {
+    const container = document.getElementById('men-products');
+    container.innerHTML = '';
+    products
+      .filter(product => product.category === 'men')
+      .forEach(product => {
+        container.innerHTML += `
+          <a href="product.html?id=${product.id}" style="text-decoration:none; color:inherit;">
+            <div class="product-card">
+              <img src="${product.image}" alt="${product.title}">
+              <h3>${product.title}</h3>
+              <p>${product.description}</p>
+              <span class="price">${product.price}</span>
+            </div>
+          </a>
+        `;
+      });
+  });
